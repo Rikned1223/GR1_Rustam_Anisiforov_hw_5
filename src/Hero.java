8d6288e722f45dcd92e9f897d0ce356b2c4fa493
Rikned1223 <yleevk@gmail.com>
2022-09-14 14:28:21 +0600

    Initial commit

---
+++ src/Hero.java
@@ -0,0 +1,21 @@
+public class Hero {
+    private int Health;
+    private int Damage;
+    private String superPower;
+
+    public Hero(int Health, int Damage, String superPower) {
+    }
+
+    public Hero(int Health, int Damage) {
+
+    }
+    public int getHealth(){
+        return Health;
+    }
+    public int getDamage(){
+        return Damage;
+    }
+    public String getSuperPower(){
+        return superPower;
+    }
+}
